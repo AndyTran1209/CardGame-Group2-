ef55caa6299059616131b3ec3f000740b7f77131
Andy <tran84@sheridancollege.ca>
2019-08-12 18:53:09 -0400

    Test for dealing cards

--- src/CardGame/test.java
+++ src/CardGame/test.java
@@ -13,8 +13,16 @@ public class test {
     
     public static void main(String[] args){
         Deck deck = new Deck(52);
+        
+        System.out.println("Beginning Size:");
+        System.out.println(deck.getSize() + "\n");
+        
+        System.out.println("Deal 15 cards:");
         for (int i = 0; i <= 15; i++)
         System.out.println(deck.dealCard());
+        System.out.println("\n");
+        
+        System.out.println("Final Size:");
         System.out.println(deck.getSize());
     }
     
