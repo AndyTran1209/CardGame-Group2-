30a3e9aaed3e2d49f06e22ac1b9171756ac9977d
Andy <tran84@sheridancollege.ca>
2019-08-13 20:20:05 -0400

    added player methods

--- src/CardGame/test.java
+++ src/CardGame/test.java
@@ -14,17 +14,26 @@ public class test {
     public static void main(String[] args) throws Exception{
         Deck deck = new Deck(52);
         
+        TwentyOneGamePlayer p = new TwentyOneGamePlayer("Player");
         
-        System.out.println("Beginning Size:");
-        System.out.println(deck.getSize() + "\n");
+        for(int i = 0; i <= 2; i++){
+            p.receive(deck.dealCard());
+            System.out.println(p.toString(i));
+        }
+        System.out.println(p.getPoints());
         
-        System.out.println("Deal 15 cards:");
-        for (int i = 0; i <= 15; i++)
-        System.out.println(deck.dealCard());
-        System.out.println("\n");
         
-        System.out.println("Final Size:");
-        System.out.println(deck.getSize());
+        
+//        System.out.println("Beginning Size:");
+//        System.out.println(deck.getSize() + "\n");
+//        
+//        System.out.println("Deal 15 cards:");
+//        for (int i = 0; i <= 15; i++)
+//        System.out.println(deck.dealCard());
+//        System.out.println("\n");
+//        
+//        System.out.println("Final Size:");
+//        System.out.println(deck.getSize());
     }
     
     
